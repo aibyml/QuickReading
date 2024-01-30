d7cbac5210a0dd43db63a31caa27b874a65d3773
Victor HUNG <cklbckoho@users.noreply.huggingface.co>
2024-01-30 06:28:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ def load_docspdf(directory):
 directory = 'data'
 documents = load_docs(directory)
 
-loading = st.button("Upload docs")
-if loading: 
+st.button("Upload docs")
+if button:
     
     documents = st.file_uploader("Upload documents here, only PDF file allowed", type=["pdf"], accept_multiple_files=False)
     submit=st.button("Help me base on the doc to help me understand the content")
