74bf0f2ae12a9e861a0ebb7a3b1abda2a5186bf0
Victor HUNG <cklbckoho@users.noreply.huggingface.co>
2024-01-30 08:05:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain import HuggingFaceHub
 
 #This module provides a way to interact with the operating system, such as accessing environment variables, working with files
 #and directories, executing shell commands, etc
-import pyPDF2
+from PyPDF2 import PdfReader
 import os
 
 #By st.set_page_config(), you can customize the appearance of your Streamlit application's web page
