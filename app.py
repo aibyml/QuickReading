e37f8a78d7c06bbadc20cc797b3590a16d5986a6
Victor HUNG <cklbckoho@users.noreply.huggingface.co>
2024-02-01 15:36:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -116,12 +116,12 @@ def QA(documents):
         st.session_state.generated = []
         st.session_state.db = None
 
+with st.button ("$upload$ $docs$"):
+    documents = st.file_uploader("Upload documents here, only PDF file allowed", type=["pdf"], accept_multiple_files=True)
+    st.write(documents)
+    #st.write("Approx number of token", len(docs))
+    QA(documents)    
+    
 directory = 'data'
 documents = load_docs(directory)
 QA(documents)
-
-if st.button ("$upload$ $docs$"):
-    doc = st.file_uploader("Upload documents here, only PDF file allowed", type=["pdf"], accept_multiple_files=True)
-    st.write(doc)
-    st.write(documents)
-    #st.write("Approx number of token", len(docs))
